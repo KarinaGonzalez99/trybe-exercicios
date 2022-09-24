b58c46faaa1d096e8e80bf22b8cb98f42c7cd3d4
Kaka <kaahgonzalez99@gmail.com>
2022-09-24 16:51:55 -0300

    exercicios logica

---
+++ fundamentos/04-introducao-javascript/03-logica-algoritmos/script-inverso.js
@@ -0,0 +1,24 @@
+let word = 'tryber';
+let reverseWord = '';
+
+for (let index = 0; index < word.length; index += 1) {
+  reverseWord += word[word.length - 1 - index];
+}
+
+console.log(reverseWord);
+
+
+
+
+
+ou 
+
+
+
+
+let word = 'tryber';
+let reverseWord = '';
+
+reverseWord = word.split('').reverse().join('');
+
+console.log(reverseWord);
